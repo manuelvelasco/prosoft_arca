4f3e17c29279b1bc81c9c6cc26e244fc671a3e14
mvelasco <mvelasco@localhost>
2024-10-29 16:14:37 -0600

    * ARCA - Habilitación de ambiente QA

--- backend/comunes/funciones.php
+++ backend/comunes/funciones.php
@@ -222,6 +222,19 @@
      */
 
 
+    function generaCadenaAleatoria($length = 3) {
+        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $charactersLength = strlen($characters);
+        $randomString = '';
+
+        for ($i = 0; $i < $length; $i++) {
+            $randomString .= $characters[random_int(0, $charactersLength - 1)];
+        }
+
+        return $randomString;
+    }
+
+
     function calculaTiempoDeInactividad($fechaInicial, $fechaFinal) {
         $tiempoInactividad = $fechaFinal->diff($fechaInicial);
         return $tiempoInactividad->d . " dias, " . $tiempoInactividad->h . " horas, " . $tiempoInactividad->i . " minutos";
