8085c68e1fad55203b0f693005c9fa97ba13ad92
mvelasco <mvelasco@localhost>
2026-04-16 08:24:06 -0600

    * ARCA - Ajuste de rutas para mensajeros

--- cms/socialware/php/comunes/constantes.php
+++ cms/socialware/php/comunes/constantes.php
@@ -1,27 +1,39 @@
 <?php
     $charset = "utf-8";
 
-    //$constante_rutaVehiculos = "/Users/mvelasco/Socialware/Proyectos/Web/arca/vehiculos/";
-    $constante_rutaVehiculos = "/var/www/html/arca/vehiculos/";
-    //$constante_rutaVehiculos = "/var/www/html/arca/vehiculos/";
-
-    //$constante_urlVehiculos = "http://localhost/web/arca/vehiculos/";
-    $constante_urlVehiculos = "http://54.196.77.109/arca/vehiculos/";
-    //$constante_urlVehiculos = "https://seminuevosarca.com.mx/vehiculos/";
+    // Concesionarios
 
     //$constante_rutaConcesionarios = "/Users/mvelasco/Socialware/Proyectos/Web/arca/concesionarios/";
     $constante_rutaConcesionarios = "/var/www/html/arca/concesionarios/";
-    //$constante_rutaConcesionarios = "/var/www/html/arca/concesionarios/";
 
     //$constante_urlConcesionarios = "http://localhost/web/arca/concesionarios/";
     $constante_urlConcesionarios = "http://54.196.77.109/arca/concesionarios/";
     //$constante_urlConcesionarios = "https://seminuevosarca.com.mx/concesionarios/";
 
+    // Mensajeros
+
+    //$constante_rutaMensajeros = "/Users/mvelasco/Socialware/Proyectos/Web/arca/mensajeros/";
+    $constante_rutaMensajeros = "/var/www/html/arca/mensajeros/";
+
+    //$constante_urlMensajeros = "http://localhost/web/arca/mensajeros/";
+    $constante_urlMensajeros= "http://54.196.77.109/arca/mensajeros/";
+    //$constante_urlMensajeros = "https://seminuevosarca.com.mx/mensajeros/";
+
+    // Posts
+
     //$constante_rutaPosts = "/Users/mvelasco/Socialware/Proyectos/Web/arca/posts/";
     $constante_rutaPosts = "/var/www/html/arca/posts/";
-    //$constante_rutaPosts = "/var/www/html/arca/posts/";
 
     //$constante_urlPosts = "http://localhost/web/arca/posts/";
     $constante_urlPosts = "http://54.196.77.109/arca/posts/";
     //$constante_urlPosts = "https://seminuevosarca.com.mx/posts/";
+
+    // Vehiculos
+
+    //$constante_rutaVehiculos = "/Users/mvelasco/Socialware/Proyectos/Web/arca/vehiculos/";
+    $constante_rutaVehiculos = "/var/www/html/arca/vehiculos/";
+
+    //$constante_urlVehiculos = "http://localhost/web/arca/vehiculos/";
+    $constante_urlVehiculos = "http://54.196.77.109/arca/vehiculos/";
+    //$constante_urlVehiculos = "https://seminuevosarca.com.mx/vehiculos/";
 ?>
