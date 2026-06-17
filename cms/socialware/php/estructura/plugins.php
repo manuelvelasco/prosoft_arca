635043fe576d9336dd6e871fc151eefce024a342
mvelasco <mvelasco@localhost>
2026-06-17 08:59:29 -0600

    * ARCA > CMS - Agregar al proyecto el plugin jQuery LoadingOverlay

--- cms/socialware/php/estructura/plugins.php
+++ cms/socialware/php/estructura/plugins.php
@@ -132,6 +132,12 @@
         -->
         <script src="https://cdn.jsdelivr.net/npm/@fancyapps/ui@4.0/dist/fancybox.umd.js"></script>
 
+        <!--
+        jQuery LoadingOverlay
+        https://gasparesganga.com/labs/jquery-loading-overlay/
+        -->
+        <script src="https://cdn.jsdelivr.net/npm/gasparesganga-jquery-loading-overlay@2.1.7/dist/loadingoverlay.min.js"></script>
+
         <!-- CUSTOM JS -->
         <script src="assets/js/custom.js"></script>
 
